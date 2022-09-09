dfc8e31d5d8e087ff0610bc6735607551e8099d5
devhindo <66886118+devhindo@users.noreply.github.com>
2022-09-09 21:00:26 +0200

    Time: 46 ms (6.54%), Space: 8.4 MB (15.29%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         int integer = 0;
-        map<char,int> dic;
-        string symbols = "IVXLCDM";
-        int values[] = {1,5,10,50,100,500,1000};
-        for(int i=0; i<7; i++) {
-            dic[symbols[i]] = values[i];
-        }
+        map<char,int> dic { {'I',1}, {'V',5}, {'X',10}, {'L', 50}, {'C',100},{'D', 500}, {'M', 1000} };
         int j=0;
         while(j<s.length()) {
             if(dic[s[j+1]] && dic[s[j]] < dic[s[j+1]]) {
