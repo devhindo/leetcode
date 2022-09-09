f1e3f88ddd6a76be031b7437603133c36ecbf791
devhindo <66886118+devhindo@users.noreply.github.com>
2022-09-09 21:35:22 +0200

    Time: 44 ms (7.78%), Space: 8.5 MB (15.29%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         int integer = 0;
-        unordered_map<char,int> dic { {'I',1}, {'V',5}, {'X',10}, {'L', 50}, {'C',100},{'D', 500}, {'M', 1000} };
+        map<char,int> dic { {'I',1}, {'V',5}, {'X',10}, {'L', 50}, {'C',100},{'D', 500}, {'M', 1000} };
         int j=0;
         while(j<s.length()) {
             if(dic[s[j+1]] && dic[s[j]] < dic[s[j+1]]) {
