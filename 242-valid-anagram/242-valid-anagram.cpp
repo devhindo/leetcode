c783df02450136b9f749f82c83b44c01a371b4fc
devhindo <66886118+devhindo@users.noreply.github.com>
2022-09-12 22:13:39 +0200

    Time: 10 ms (74.41%), Space: 7.4 MB (9.18%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,20 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) {
-            return false;
-        }
-        int l = s.length();
-        map<char,int>m;
-        for(auto i=0; i<l; i++) {
-            m[s[i]]++;
-            m[t[i]]--;
-        }
-        for(auto itr = m.begin(); itr != m.end(); itr++) {
-            if(itr->second != 0) {
-                return false;
-            }
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return (s==t) ? true : false;
     }
 };
