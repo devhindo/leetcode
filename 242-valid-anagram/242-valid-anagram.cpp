58750c26a85222dbfb9a1dc8678046244167221f
devhindo <66886118+devhindo@users.noreply.github.com>
2022-09-12 22:00:05 +0200

    Time: 33 ms (5.19%), Space: 7.5 MB (9.18%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,27 +4,17 @@ public:
         if(s.length() != t.length()) {
             return false;
         }
-        bool f = true;
-        map<char,int>m;
-        for(char c : s) {
-            m[c]++;
+        int l = s.length();
+        map<char,int>m = { {'a',0}, {'b',0}, {'c',0}, {'d',0}, {'e',0}, {'f',0}, {'g',0}, {'h',0},                   {'i',0}, {'j',0}, {'k',0}, {'l',0}, {'m',0}, {'n',0}, {'o',0}, {'p',0}, {'q',0}, {'r',0}, {'s',0},           {'t',0}, {'u',0}, {'v',0}, {'w',0}, {'x',0}, {'y',0}, {'z',0} };
+        for(auto i=0; i<l; i++) {
+            m[s[i]]++;
+            m[t[i]]--;
         }
-        for(char c : t) {
-            if(m.find(c) != m.end()) {
-                m[c]--;
-                if(m[c] < 0) {
-                    return false;
-                }
-            } else {
+        for(auto itr = m.begin(); itr != m.end(); itr++) {
+            if(itr->second != 0) {
                 return false;
-            }         
-        }
-        for(auto itr=m.begin(); itr!=m.end(); itr++) {
-            if (itr->second != 0) {
-                f=false;
-                break;
             }
         }
-        return f;
+        return true;
     }
 };
