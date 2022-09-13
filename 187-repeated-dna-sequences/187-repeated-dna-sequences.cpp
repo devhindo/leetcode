fdae2bc961b53dea97c6c25e1b8a5233d611dc4a
devhindo <66886118+devhindo@users.noreply.github.com>
2022-09-14 00:39:01 +0200

    Time: 359 ms (5.00%), Space: 22.3 MB (68.00%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -3,7 +3,7 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         vector<string>r;
         if(s.size() < 10) return r;
-        unordered_map<string,int>dnas;
+        map<string,int>dnas;
         for(auto i=0; i<s.size()-9; i++) {
             string q = s.substr(i,10);
             dnas[q]++;
