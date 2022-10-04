887c116a85c803109dc4480df0b22b25e34f5e95
devhindo <66886118+devhindo@users.noreply.github.com>
2022-10-04 18:38:24 +0200

    Time: 245 ms (14.85%), Space: 46.5 MB (97.67%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int>s;
-        for(int i=0; i<nums.size(); i++) {
-            if(s.find(nums[i]) != s.end()) {
-                return true;
-            } else {
-                s.insert(nums[i]);
-            }
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<nums.size()-1; i++) {
+            if(nums[i] == nums[i+1]) return true;
         }
         return false;
     }
