374db91e94696b7e8b141ea474584682a07d29ff
devhindo <66886118+devhindo@users.noreply.github.com>
2022-10-06 01:07:33 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (8.49%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int fib(int n, unordered_map<int,int> *m = new unordered_map<int,int>()) {
-        // Dynamic Programming #1 :)
         if((*m).find(n) != (*m).end()) return (*m)[n];
         if(n == 0) return 0;
         if(n == 1) return 1;
