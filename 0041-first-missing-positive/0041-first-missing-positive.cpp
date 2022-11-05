98ee98c822ca5f9a54833652b2b1033d100e41ac
devhindo <66886118+devhindo@users.noreply.github.com>
2022-11-05 16:28:39 +0200

    Time: 301 ms (5.12%), Space: 47.3 MB (11.70%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        set<int>s(nums.begin(),nums.end());
-        int i=0, c=1;
-        for(auto itr=s.begin(); itr != s.end(); itr++) {
-            if(*itr <= 0)
-                continue;
-            if(*itr != c) {
-                return c;
+        int m=1;
+        set<int>s;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] <= 0) continue;
+            if(s.find(nums[i]) != s.end()) continue;
+            if(nums[i] > m) {
+                s.insert(nums[i]);
+            } else if(nums[i] == m) {
+                m++;
             }
-            c++;
         }
-        return c;
+        for(auto itr = s.begin(); itr != s.end(); itr++) {
+            if(*itr == m) {
+                m++;
+            } else {
+                return m;
+            }
+        }
+        return m;
     }
 };
