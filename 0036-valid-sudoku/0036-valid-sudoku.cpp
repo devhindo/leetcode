3e158359efb0b2f0cce756b6936e7c46439d9159
devhindo <66886118+devhindo@users.noreply.github.com>
2022-11-26 09:29:45 +0200

    Time: 27 ms (85.12%), Space: 20.9 MB (12.12%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool sqr(vector<vector<char>>& board, int rowStart, int colStart, unordered_map<int,set<int>>&c, unordered_map<int,set<int>>&r) {
+        set<int>s;
+        for(int i=rowStart; i<3+rowStart; i++) {
+            for(int j=colStart; j < colStart+3; j++) {
+                if(board[i][j] != '.') {
+                    if(s.find(board[i][j]) == s.end()) {
+                        s.insert(board[i][j]);
+                    } else {
+                        return false;
+                    }
+                    if(c[j].find(board[i][j]) == c[j].end()) {
+                        c[j].insert(board[i][j]);
+                    } else {
+                        return false;
+                    }
+                    if(r[i].find(board[i][j]) == r[i].end()) {
+                        r[i].insert(board[i][j]);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int,set<int>>c;
+        unordered_map<int,set<int>>r;
+        for(int i=0; i<9; i=i+3) {
+            for(int j=0; j<9; j=j+3) {
+                if(!sqr(board,i,j,c,r)) return false;
+            }
+        }
+        return true;
+    }
+};
