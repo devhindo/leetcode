80ca8693240d2a69708ad2379631b88d97e8683e
devhindo <66886118+devhindo@users.noreply.github.com>
2022-12-02 14:21:59 +0200

    Time: 161 ms (72.46%), Space: 21.1 MB (6.59%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -4,8 +4,6 @@ public:
         map<char,int>m1,m2;
         for(auto c : word1) m1[c]++;
         for(auto c : word2) m2[c]++;
-        if(word1.size() != word2.size() || m1.size() != m2.size()) return false;
-        if(m1 == m2) return true;
         vector<int>w1,w2;
         for(int i=97; i<=122; i++) {
             char x = i;
