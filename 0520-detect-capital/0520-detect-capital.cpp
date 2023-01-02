5dde70d8fe4b640b7bc42b9973a9b5f030af35cc
devhindo <66886118+devhindo@users.noreply.github.com>
2023-01-02 04:20:15 +0200

    Time: 0 ms (100.00%), Space: 6 MB (89.31%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size() == 1) {
+            return true;
+        } else {
+            if(isupper(word[0])) {
+                if(isupper(word[1])) {
+                    for(int i=2; i<word.size(); i++) {
+                        if(islower(word[i])) return false;
+                    }
+                } else {
+                    for(int i=2; i<word.size(); i++) {
+                        if(isupper(word[i])) return false;
+                    }
+                }
+            } else {
+                for(int i=1; i<word.size(); i++) {
+                    if(isupper(word[i])) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
