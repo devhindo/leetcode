936d873543f45748edf0151cfa042caf5873d6fb
devhindo <66886118+devhindo@users.noreply.github.com>
2023-01-04 06:24:53 +0200

    Time: 303 ms (61.16%), Space: 104.6 MB (34.09%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -5,10 +5,10 @@ public:
         int c=0;
         for(auto t : tasks) m[t]++;
         for(auto& t : m) {
-            if(t.second==1) return -1;
-            else if(t.second==2) c++;
-            else if(t.second%3==0) c += t.second/3;
-            else c += (t.second-4)/3 + 2;   
+            if(t.second==1) {return -1;}
+            else if(t.second==2) {c++;}
+            else if(t.second%3==0) {c += t.second/3;}
+            else {c += (t.second-4)/3 + 2;}   
         }
         return c;
     }
