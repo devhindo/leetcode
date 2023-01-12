848d918d30add48d417013ecee132ff0bc33e503
devhindo <66886118+devhindo@users.noreply.github.com>
2023-01-12 15:00:37 +0200

    Time: 30 ms (70.45%), Space: 23.9 MB (88.90%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int p=1;
         int z=0;
-        for(int i=0; i<nums.size(); i++) {
+        for(int i=0; i<size(nums); i++) {
             if(nums[i]==0) {
                 z++;
             } else {
@@ -11,11 +11,10 @@ public:
             }
         }
         if(z>1) {
-            vector<int>v(nums.size());
-            return v;
+            return vector<int>(size(nums));
             
         } else if(z==1) {
-            for(int i=0; i<nums.size(); i++) {
+            for(int i=0; i<size(nums); i++) {
                 if(nums[i]) nums[i]=0;
                 else {
                     nums[i]=p;
