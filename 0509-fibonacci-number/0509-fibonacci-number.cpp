76bc8cb51b179ed4c50fd7a2f90e7fae44556d91
devhindo <66886118+devhindo@users.noreply.github.com>
2023-04-25 10:04:04 +0200

    Time: 4 ms (51.14%), Space: 6.4 MB (10.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0) return 0;
-        int f;
-        if(n<=2) f=1;
-        else return f=fib(n-1)+fib(n-2);
-        return f;
+    int fib(int n, unordered_map<int,int> *m = new unordered_map<int,int>()) {
+        if((*m).find(n) != (*m).end()) return (*m)[n];
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        (*m)[n] = fib(n-1,m) + fib(n-2,m);
+        return (*m)[n];
     }
 };
