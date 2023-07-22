c53c6996bde715efafac49dd4a738fca3029d93b
devhindo <66886118+devhindo@users.noreply.github.com>
2023-07-22 08:42:54 +0300

    Time: 110 ms (6.27%), Space: 16.5 MB (17.96%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        dic = {}
+        for email in emails:
+            em = ''
+            for i in range(len(email)):
+                if email[i] == '.':
+                    continue
+                elif email[i] == '+':
+                    while email[i] != '@':
+                        i += 1
+                    em += email[i:len(email)]
+                    break
+                elif email[i] == '@':
+                    em += email[i:len(email)]
+                    break
+
+                else:
+                    em += email[i]
+            dic[em] = dic.setdefault(em,0)+1
+        return len(dic)
+                
+        
+        
