4b498be35bc5df4ae3c7089239679bc695a47c5d
devhindo <66886118+devhindo@users.noreply.github.com>
2023-08-29 22:24:42 +0300

    Time: 82 ms (50.79%), Space: 21 MB (22.83%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -29,3 +29,56 @@ class Solution:
             
             
         return search(root)
+    
+    
+""" general tree solution - not a binary one
+
+def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        pf = False
+        qf = False
+        
+        pl = []
+        ql = []
+        
+        def search(n,l):
+            nonlocal pf
+            nonlocal qf
+            if pf and qf:
+                return
+            
+            l.append(n)
+            
+            if n == p:
+                nonlocal pl
+                pl = l.copy()
+                pf = True
+                
+            if n == q:
+                nonlocal ql
+                ql = l.copy()
+                qf = True
+            
+            if n.left:
+                search(n.left,l)
+                
+            if n.right:
+                search(n.right,l)
+                
+            l.pop()
+        
+        search(root, [])
+        
+        limit = min(len(pl),len(ql))
+        
+        j = 0
+        
+        while j<limit:
+            
+            if pl[j] != ql[j]:
+                return pl[j-1]
+            j += 1
+        
+        return pl[limit-1]
+
+"""
