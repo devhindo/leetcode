50af61f1fc3f02fe7ef1c0e727b02c2d43d8667b
devhindo <66886118+devhindo@users.noreply.github.com>
2023-09-13 06:37:52 +0300

    Time: 42 ms (47.48%), Space: 16.4 MB (35.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        r1, r2 = 0,0
+        
+        for n in nums:
+            t = r2
+            r2 = max(r2,r1+n)
+            r1 = t
+        
+        return r2
+
+"""
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
         dp = [0] * (len(nums)+2)
         
         for i in range(2,len(dp)):    
             dp[i] = max(dp[i-1],dp[i-2]+nums[i-2])
         
         return dp[-1]
+"""
