8f1c8acd8fe799356231accdfbfa2768fbeae93d
devhindo <lildevhind@gmail.com>
2024-05-19 06:59:16 +0300

    Time: 52 ms (89.60%), Space: 17.8 MB (16.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,7 +8,5 @@ class Solution:
                 return [i, d[target-nums[i]]]
             d[nums[i]] = i
         
-        for i in range(len(nums)):
-            if target-nums[i] in d and i != d[target-nums[i]]:
-                return [i, d[target-nums[i]]]
+
         
