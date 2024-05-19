fe3c99ac447ad00a09777890b812dd88b194c1bb
devhindo <lildevhind@gmail.com>
2024-05-19 07:14:56 +0300

    Time: 82 ms (87.33%), Space: 20.5 MB (41.78%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        d = {}
+        
+        for i in range(len(strs)):
+            st = tuple(sorted(strs[i]))
+                    
+            if st not in d:
+               d[st] = []
+            
+            d[st].append(strs[i])
+            
+        a = []
+        
+        for e in d:
+            a.append(d[e])
+            
+        return a
+        
