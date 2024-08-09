0cee12eb449e1d01afa76fe575dc17611ea7b135
devhindo <lildevhind@gmail.com>
2024-08-10 02:41:53 +0300

    Time: 41 ms (60.1%), Space: 16.5 MB (85.86%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -14,7 +14,7 @@ class Solution:
                 
                 c1 = grid[i][j]+grid[i+1][j]+grid[i+2][j]
                 c2 = grid[i][j+1]+grid[i+1][j+1]+grid[i+2][j+1]
-                c3 = grid[i][j+2]+grid[i+1][j+2]+grid[i+2][j+2]\
+                c3 = grid[i][j+2]+grid[i+1][j+2]+grid[i+2][j+2]
                 
                 s = set()
                 f = False
